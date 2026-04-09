2ed95b7c211f5e619d781bfbd1a1ce5d6e09fa71
sonu7042 <sonusinghmourya100@gmail.com>
2026-04-09 17:51:37 +0530

    updted

--- server/index.js
+++ server/index.js
@@ -8,11 +8,13 @@ const postRoutes = require('./routes/postRoutes');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
+//   })
+// );
+
+app.use(cors());  
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/api/auth', authRoutes);
