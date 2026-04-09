e373a7ef05fbcb4ae4a918cdc0e4aa66d9ec663b
sonu7042 <sonusinghmourya100@gmail.com>
2026-04-09 18:04:46 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -6,31 +6,26 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const postRoutes = require('./routes/postRoutes');
 const app = express();
+connectDB();
 const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "https://community-henna.vercel.app",
-    credentials: true,
+    origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
   })
 );
-
-// app.use(cors());  
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-const startServer = async () => {
-  await connectDB();
 
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
-};
-
-startServer();
+module.exports = app;
