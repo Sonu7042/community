cea768eeeb26c34187595b48b64741498f6cb0f7
sonu7042 <sonusinghmourya100@gmail.com>
2026-04-09 17:58:19 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -8,13 +8,14 @@ const postRoutes = require('./routes/postRoutes');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://community-henna.vercel.app",
+    credentials: true,
+  })
+);
 
-app.use(cors());  
+// app.use(cors());  
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/api/auth', authRoutes);
