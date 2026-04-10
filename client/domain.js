fcfed299ea66a95928104e197f7c16091c89307a
sonu7042 <sonusinghmourya100@gmail.com>
2026-04-10 16:00:44 +0530

    updated

--- client/domain.js
+++ client/domain.js
@@ -1,11 +1,11 @@
-export const  API_BASE_URL = 'http://localhost:3000/api';
-export const  API_BASE_URL_Auth = 'http://localhost:3000/api/auth';
+// export const  API_BASE_URL = 'http://localhost:3000/api';
+// export const  API_BASE_URL_Auth = 'http://localhost:3000/api/auth';
 
 
 
 
 // <----------------- for live--------------------->
-// export const  API_BASE_URL = 'https://community-j5fs.vercel.app/api';
-// export const  API_BASE_URL_Auth = 'https://community-j5fs.vercel.app/api/auth';
+export const  API_BASE_URL = 'https://community-j5fs.vercel.app/api';
+export const  API_BASE_URL_Auth = 'https://community-j5fs.vercel.app/api/auth';
 
 
